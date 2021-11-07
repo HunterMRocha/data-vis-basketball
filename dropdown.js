848c1f930c849edbddc5d8ab6999b7be2f6a6733
HunterMRocha <Huntermacias20@gmail.com>
2021-11-07 13:38:30 -0800

    added simple dropdown ex.

---
+++ dropdown.js
@@ -0,0 +1,19 @@
+var players = ["Lebron James", "Stephen Curry", "Derrick Rose"]
+
+var select = d3.select('body')
+  .append('select')
+  	.attr('class','select')
+    .on('change',onchange)
+
+var options = select
+  .selectAll('option')
+	.data(players).enter()
+	.append('option')
+		.text(function (d) { return d; });
+
+function onchange() {
+	selectValue = d3.select('select').property('value')
+	d3.select('body')
+		.append('p')
+		.text(selectValue + ' is the last selected option.')
+};
